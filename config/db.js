c60e0fe90914551df86ae84303ec67ff8ffeddb8
Jhoel Ccari Quispe <jhoelccari.23@gmail.com>
2019-09-05 17:07:13 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 module.exports = {
-    dbuser : "jhoelccari",
-    dbpassword : "YmibOMgJ5LPI8VVJ",
+    dbuser : "",        // set userdb
+    dbpassword : "",    // set password
     dbUrl : function (){
-        var url = "mongodb+srv://"+ this.dbuser+":"+ this.dbpassword + "@clustergastospersonales-gqibo.gcp.mongodb.net/gastos-personales?retryWrites=true"
+        var url = "";   // set url database connection like:
+                        // var url = "mongodb+srv://"+ this.dbuser+":"+ this.dbpassword + "@clustergastospersonales-gqibo.gcp.mongodb.net/gastos-personales?retryWrites=true"                   
         return url;
     }
 }
